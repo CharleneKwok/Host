7ea89f7979ee8994a695dc7f1d1fb9ba29ddd037
Charlene Kwok <charlenekwok499@gmail.com>
2021-12-19 23:15:50 +1100

    add mobile nav bar

--- shared.js
+++ shared.js
@@ -4,6 +4,8 @@ const modal = document.getElementsByClassName("modal")[0];
 const buttons = document.getElementsByClassName("plan-card__button");
 // const buttons = document.querySelectorAll('plan-card__button');
 const noButton = document.getElementsByClassName("modal__action--negative")[0];
+const toggle = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
 
 [...buttons].forEach(button => {
     button.addEventListener('click', () => {
@@ -17,7 +19,21 @@ const closeModal = () => {
     modal.style.display = 'none';
 }
 
-noButton.addEventListener('click', closeModal);
-backdrop.addEventListener('click', closeModal);
+if (noButton) {
+    noButton.addEventListener('click', closeModal);
+}
+
+backdrop.addEventListener('click', () => {
+    if (modal) {
+        modal.style.display = 'none';
+    }
+    backdrop.style.display = 'none';
+    mobileNav.style.display = 'none';
+});
+
+toggle.addEventListener('click', () => {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
 
 
